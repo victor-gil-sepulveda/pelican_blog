1acd2b5aebc7fdea543852c0482a4edf3ba685fc
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-10-08 14:17:19 +0200

    Added auto_quickstart

--- scripts/vagrant/provision.sh
+++ scripts/vagrant/provision.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install expect
+pip install pelican
+pip install fabric
+pip install ghp-import
+
+mkdir blog
+python /pelican/scripts/auto_quickstart.py
+ln -s /vagrant/contents blog/contents
