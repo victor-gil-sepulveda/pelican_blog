dd156358cf10ba9759b2c28c7541b7ca66093d6c
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-10-11 20:13:30 +0200

    repo page almost working. Promises must be improved

--- data/theme/static/js/github_repos.js
+++ data/theme/static/js/github_repos.js
@@ -1,3 +1,5 @@
+var pruned_repos = new Array();
+
 function get_repos_from_github(){
     var github_api_url = "https://api.github.com/users/victor-gil-sepulveda/repos";
     return html_get(github_api_url, process_repo_response);
@@ -5,10 +7,11 @@ function get_repos_from_github(){
 
 function html_get(url, callback){
     var http_handler = new XMLHttpRequest();
-    http_handler.onreadystatechange = function() { 
-        if (http_handler.readyState == 4 && http_handler.status == 200)
+    http_handler.onreadystatechange = function(){ 
+        if (http_handler.readyState == 4 && http_handler.status == 200){
             callback(http_handler.responseText);
-    }
+        }
+    };
     http_handler.open("GET", url, true);  
     http_handler.send(null);
 }
@@ -28,7 +31,7 @@ function html_get_languages(url, repo, deferred){
         else{
             deferred.reject();
         }
-    }
+    };
     http_handler.open("GET", url, true);  
     http_handler.send(null);
 }
@@ -37,15 +40,15 @@ function process_repo_response(response){
      var repos = JSON.parse(response);
      
      // Order by stars
-     repos.sort(function(x,y){return x.stargazers_count > y.stargazers_count;})
+     repos.sort(function(x,y){return x.stargazers_count > y.stargazers_count;});
      
      // Get interesting stuff from each object
      var i;
-     var pruned_repos = new Array()
      var promises = new Array();
      for ( i = 0; i < repos.length; i++){
         var repo = repos[i];
         var new_repo = {};
+        new_repo["name"] = repo["name"];
         new_repo["link"] = repo["html_url"];
         new_repo["stars"] = repo["stargazers_count"];
         new_repo["description"] = repo["description"];
@@ -61,11 +64,13 @@ function process_repo_response(response){
      return promises;
 }
 
-$(document).ready(funtion(){
-    $.when.apply(null,get_repos_from_github()).done(function(){
-        var source   = $("#entry-template").html();
+$(document).ready(function(){
+    $.when.apply(null,get_repos_from_github()).then(function(){
+        var source   = $("#repos_template").html();
         var template = Handlebars.compile(source);
-        var context = {title: "My New Post", body: "This is my first post!"};
+        console.log(pruned_repos);
+        var context = {repos: pruned_repos};
         var html    = template(context);
-    }
-})
+        $("#contents-page").html(html);
+    });
+});
