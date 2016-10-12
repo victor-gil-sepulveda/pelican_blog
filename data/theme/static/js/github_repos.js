0a283b04cc364e1d765bbdd3442f0930d1e9fadb
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-10-12 11:17:31 +0200

    added github and bitbucket icons, and repo card css

--- data/theme/static/js/github_repos.js
+++ data/theme/static/js/github_repos.js
@@ -1,51 +1,13 @@
 var pruned_repos = new Array();
 
-function get_repos_from_github(){
-    var github_api_url = "https://api.github.com/users/victor-gil-sepulveda/repos";
-    return html_get(github_api_url, process_repo_response);
-}
-
-function html_get(url, callback){
-    var http_handler = new XMLHttpRequest();
-    http_handler.onreadystatechange = function(){ 
-        if (http_handler.readyState == 4 && http_handler.status == 200){
-            callback(http_handler.responseText);
-        }
-    };
-    http_handler.open("GET", url, true);  
-    http_handler.send(null);
-}
-
-function html_get_languages(url, repo, deferred){
-    var http_handler = new XMLHttpRequest();
-    http_handler.onreadystatechange = function() { 
-        if (http_handler.readyState == 4 && http_handler.status == 200){
-            try{
-                repo["all_languages"] = JSON.parse(http_handler.responseText);
-                deferred.resolve();
-            }
-            catch(e){
-                deferred.reject();
-            }
-        }
-        else{
-            deferred.reject();
-        }
-    };
-    http_handler.open("GET", url, true);  
-    http_handler.send(null);
-}
-
-function process_repo_response(response){
-     var repos = JSON.parse(response);
-     
+function process_github_repos(repos){
      // Order by stars
      repos.sort(function(x,y){return x.stargazers_count < y.stargazers_count;});
      
      // Get interesting stuff from each object
      var i;
-     var promises = new Array();
      var name;
+     var promises = new Array();
      for ( i = 0; i < repos.length; i++){
         var repo = repos[i];
         if(!repo.fork){
@@ -63,25 +25,82 @@ function process_repo_response(response){
             }
             new_repo["description"] = description;
             new_repo["main_language"] = repo["language"];
-            var deferred = $.Deferred();
-            html_get_languages(repo["languages_url"], new_repo, deferred);
-            promises.push(deferred.promise());
+            new_repo["type"] = "github";
+            
+            promises.push($.getJSON( repo["languages_url"], function( data ) {
+                new_repo["all_languages"] = data;
+            }));
+            
          	pruned_repos.push(new_repo);
          }
      }
      
      console.log(pruned_repos);
-     
      return promises;
 }
 
-$(document).ready(function(){
-    $.when.apply(null,get_repos_from_github()).then(function(){
+function get_repos_from_github(){
+    var GITHUB_API_URL = "https://api.github.com/users/victor-gil-sepulveda/repos";
+    $.getJSON(GITHUB_API_URL)
+    .done(function( data ) {
+        var promises = process_github_repos(data);
+        
+        $.when.apply($, promises).then(function(){
+            var source   = $("#repos_template").html();
+            var template = Handlebars.compile(source);
+            var context = {repos: pruned_repos};
+            var html    = template(context);
+            $("#contents-page").html(html);
+        });
+    })
+    .fail(function(){console.log("Failed to retrieve Github repositories");});
+}
+
+function process_bitbucket_repos(repos){
+
+     // Get interesting stuff from each object
+     var i;
+     var name;
+     for ( i = 0; i < repos["values"].length; i++){
+        var repo = repos["values"][i];
+        if(!repo.fork){
+            var new_repo = {};
+            name =  repo["name"];
+            if (name.length >20){
+                name = name.substring(0,19)+"...";
+            }
+            new_repo["name"] = name;
+            new_repo["link"] = repo["links"]["html"];
+            description = repo["description"];
+            if(!!description && description.length > 150){
+                description = description.substring(0,150)+"...";
+            }
+            new_repo["description"] = description;
+            new_repo["main_language"] = repo["language"];
+            new_repo["type"] = "bitbucket";
+         	pruned_repos.push(new_repo);
+         }
+     }
+     
+     console.log(pruned_repos);
+}
+
+function get_repos_from_bitbucket(){
+    var BITBUCKET_API_URL = "https://api.bitbucket.org/2.0/repositories/victor_gil_sepulveda";
+    $.getJSON(BITBUCKET_API_URL)
+    .done(function( data ) {
+        process_bitbucket_repos(data);
         var source   = $("#repos_template").html();
         var template = Handlebars.compile(source);
-        console.log(pruned_repos);
         var context = {repos: pruned_repos};
         var html    = template(context);
-        $("#contents-page").html(html);
-    });
+        $("#contents-page").append(html);
+    })
+    .fail(function(){console.log("Failed to retrieve Bitbucket repositories");});
+}
+
+$(document).ready(function(){
+    console.log("ok");
+    get_repos_from_github();
+    get_repos_from_bitbucket();
 });
